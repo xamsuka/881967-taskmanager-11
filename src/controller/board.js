691b8169f0271fb2a1df3050c7ae41b98460c0bc
Vladislav <v.kozlovsky97@gmail.com>
2020-04-26 17:22:47 +0600

    Добавление закрытия формы редактирования через ESC. Реализует контроллер управления задачами

---
+++ src/controller/board.js
@@ -0,0 +1,93 @@
+import BoardNoTaskComponent from '../components/board-no-task';
+import BoardComponent from '../components/board';
+import LoadButtonComponent from '../components/button-load';
+import FormEditTaskComponent from '../components/form-edit';
+import SortComponent from '../components/sorting';
+import TaskComponent from '../components/task';
+import TasksComponent from '../components/tasks';
+import {readerElement, replace} from '../utils/render';
+
+import Utils from '../utils/common';
+
+export default class BoardController {
+  constructor(container) {
+    this._container = container;
+    this._boardComponent = new BoardComponent();
+    this._sortComponent = new SortComponent();
+    this._tasksComponent = new TasksComponent();
+    this._formEditTaksComponent = new FormEditTaskComponent();
+    this._loadButtonComponent = new LoadButtonComponent();
+  }
+
+  renderTask(task) {
+
+    const closeEditWayPoin = () => {
+      const buttonSave = document.querySelector(`.card__save`);
+      buttonSave.click();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    };
+
+    const onEscKeyDown = (evt) => {
+      const UtilsComponent = new Utils();
+      UtilsComponent.isEscPress(evt, closeEditWayPoin);
+    };
+
+    const mountedTask = (taskComponent, taskEditComponent) => {
+      const buttonEdit = taskComponent.getElement().querySelector(`.card__btn--edit`);
+      const buttonSave = taskEditComponent.getElement().querySelector(`.card__form`);
+
+      buttonEdit.addEventListener(`click`, () => {
+        replace(taskComponent, taskEditComponent);
+        document.addEventListener(`keydown`, onEscKeyDown);
+      });
+
+      buttonSave.addEventListener(`submit`, (evt) => {
+        evt.preventDefault();
+        replace(taskEditComponent, taskComponent);
+      });
+    };
+
+    const taskComponent = new TaskComponent(task);
+    const taskEditComponent = new FormEditTaskComponent(task);
+
+    mountedTask(taskComponent, taskEditComponent);
+
+    return taskComponent;
+  }
+
+  render(tasks) {
+    const SHOWUNG_TASK_COUNT_START = 8;
+    const INCREMENT_TASK_ON_NUMBER = 8;
+    let showingTasksCount = SHOWUNG_TASK_COUNT_START;
+    const isAllArhive = Object.keys(tasks).length === 0;
+
+    if (isAllArhive) {
+      const boardNoComponent = new BoardNoTaskComponent();
+      readerElement(this._container, boardNoComponent);
+      return;
+    } else {
+      const onButtonLoadClick = (evt) => {
+        const target = evt.target;
+        const prevTaskCount = showingTasksCount;
+        showingTasksCount += INCREMENT_TASK_ON_NUMBER;
+        tasks.slice(prevTaskCount, showingTasksCount).forEach((task) => readerElement(siteBoardTaskElement, this.renderTask(task)));
+        if (showingTasksCount >= tasks.length) {
+          target.remove();
+        }
+      };
+
+      readerElement(this._container, this._boardComponent);
+      const siteBoardElement = this._container.querySelector(`.board`);
+
+      readerElement(siteBoardElement, this._sortComponent);
+      readerElement(siteBoardElement, this._tasksComponent);
+
+      const siteBoardTaskElement = siteBoardElement.querySelector(`.board__tasks`);
+
+      tasks.slice(0, showingTasksCount).forEach((task) => readerElement(siteBoardTaskElement, this.renderTask(task)));
+
+      readerElement(siteBoardElement, this._loadButtonComponent);
+      this._loadButtonComponent.getElement().addEventListener(`click`, onButtonLoadClick);
+    }
+  }
+}
--- src/utils/common.js
+++ src/utils/common.js
@@ -13,3 +13,37 @@ const formateTime = (date) => {
 };
 
 export {getRandomValue, gettingRandomBooleanValue, formateTime};
+
+export default class Utils {
+  constructor() {
+    this._ENTER_KEY = `Enter`;
+    this._ESCAPE_KEY = `Escape`;
+  }
+
+  getRandomValue(min = 0, max = 100) {
+    return Math.floor(Math.random(min) * max);
+  }
+
+  gettingRandomBooleanValue() {
+    return Math.random() > 0.5;
+  }
+
+  formateTime(date) {
+    const hourse = String(date.getHours()).padStart(2, ``);
+    const minutes = String(date.getMinutes()).padStart(2, `0`);
+    return `${hourse}:${minutes}`;
+  }
+
+  isEscPress(evt, action) {
+    if (evt.key === this._ESCAPE_KEY) {
+      action();
+    }
+  }
+
+  isEnterPress(evt, action) {
+    if (evt.key === this._ENTER_KEY) {
+      action();
+    }
+  }
+
+}
